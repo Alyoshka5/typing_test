7bd86dc98d5692517247a1c8d1da9f629f376af3
Alyoshka <alyoshkamyshko@gmail.com>
2023-04-02 20:34:33 -0700

    Create basic typing test

--- src/App.js
+++ src/App.js
@@ -1,8 +1,45 @@
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const text = "hello world";
+
   return (
-    <div></div>
+    <TextDisplay text={text} />
+  );
+}
+
+function TextDisplay({ text }) {
+  const [typingText, setTypingText] = useState({
+    typed: "",
+    currentLetter: text[0],
+    toType: text.slice(1)
+  });
+
+  const handleTyping = (e) => {
+    const key = e.key;
+    if (!(key === typingText.currentLetter)) return;
+    nextLetter();
+
+  }
+
+  const nextLetter = () => {
+    setTypingText({
+      typed: typingText.typed + typingText.currentLetter,
+      currentLetter: typingText.toType[0],
+      toType: typingText.toType.slice(1)
+    });
+    
+  }
+
+  window.addEventListener('keydown', handleTyping);
+
+  return (
+    <div>
+      <span className='typed-text'>{typingText.typed}</span>
+      <span className='current-letter'>{typingText.currentLetter}</span>
+      <span className='to-type-text'>{typingText.toType}</span>
+    </div>
   );
 }
 
